0fadd5bde05d82a5bfd215c6d65bf8a7ba5604c3
Bryan Hinton <hintonbr@hotmail.com>
2016-11-16 16:19:34 -0700

    finished Day4 of the challenge, but lost some time with having upgraded to a new version of .NET Core with the VS2017 upgrade and having that cause me some problems

---
+++ katas/c#/HackerRank/30 day challenge/Day4.cs
@@ -0,0 +1,48 @@
+using System; 
+using System.Collections.Generic;
+using System.IO;
+
+class Day4
+{
+    public void Run(String[] args)
+    {
+         int T=int.Parse(Console.In.ReadLine());
+        for (int i = 0; i < T; i++) {
+            int age=int.Parse(Console.In.ReadLine());
+            Person p=new Person(age);
+             p.amIOld();
+                for (int j = 0; j < 3; j++) {
+                  p.yearPasses();
+                }
+                p.amIOld();
+                Console.WriteLine();
+        }
+    }
+}
+
+public class Person 
+{
+    int age; 
+    public Person(int initialAge)
+    {
+        if (initialAge < 0)
+        {
+            age = 0; 
+            Console.WriteLine("Age is not valid, setting age to 0."); 
+        }
+
+        age = initialAge; 
+    }
+
+    public void yearPasses()
+    {
+        age++; 
+    }
+
+    public void amIOld() {
+        if (age < 13) Console.WriteLine("You are young.");
+        else if (age >= 13 && age < 18) Console.WriteLine("You are a teenager.");
+        else Console.WriteLine("You are old.");
+    }
+
+}
