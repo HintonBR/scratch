2bae2e1eaef52759fbce203ba020855494ac17c5
Bryan Hinton <hintonbr@hotmail.com>
2016-12-06 21:20:46 -0700

    Finished Day 7 of the challenge

---
+++ katas/c#/HackerRank/30 day challenge/Day7.cs
@@ -0,0 +1,22 @@
+using System; 
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Day7
+{
+    public void Run(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] arr_temp = Console.ReadLine().Split(' ');
+        int[] arr = Array.ConvertAll(arr_temp,Int32.Parse);
+        var reversedArray = arr.Reverse();
+        bool isFirst = true;
+        foreach(var item in reversedArray) 
+        {
+            Console.Write("{0}{1}", (!isFirst  ? " " : ""), item);
+            isFirst = false;
+        }
+    }
+}
+
