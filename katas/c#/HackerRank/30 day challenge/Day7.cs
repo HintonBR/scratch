78420fb5a3431ae566881b7bba0a043a61792490
Bryan Hinton <HintonBR@hotmail.com>
2017-05-08 12:03:30 -0600

    Updated to work with the new version of .NET Core

--- katas/c#/HackerRank/30 day challenge/Day7.cs
+++ katas/c#/HackerRank/30 day challenge/Day7.cs
@@ -7,16 +7,16 @@ class Day7
 {
     public void Run(String[] args)
     {
-        int n = Convert.ToInt32(Console.ReadLine());
-        string[] arr_temp = Console.ReadLine().Split(' ');
-        int[] arr = Array.ConvertAll(arr_temp,Int32.Parse);
-        var reversedArray = arr.Reverse();
-        bool isFirst = true;
-        foreach(var item in reversedArray) 
-        {
-            Console.Write("{0}{1}", (!isFirst  ? " " : ""), item);
-            isFirst = false;
-        }
+        // int n = Convert.ToInt32(Console.ReadLine());
+        // string[] arr_temp = Console.ReadLine().Split(' ');
+        // int[] arr = Array.ConvertAll(arr_temp,Int32.Parse);
+        // var reversedArray = arr.Reverse();
+        // bool isFirst = true;
+        // foreach(var item in reversedArray) 
+        // {
+        //     Console.Write("{0}{1}", (!isFirst  ? " " : ""), item);
+        //     isFirst = false;
+        // }
     }
 }
 
