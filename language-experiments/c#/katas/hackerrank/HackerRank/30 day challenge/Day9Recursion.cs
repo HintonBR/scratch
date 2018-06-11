c178fc6de2e221a0dc937779444b358f0b2594ec
Bryan Hinton <hintonbr@hotmail.com>
2018-06-11 14:31:11 -0600

    Redid Day 9 and did day 10

---
+++ language-experiments/c#/katas/hackerrank/HackerRank/30 day challenge/Day9Recursion.cs
@@ -0,0 +1,26 @@
+using System; 
+using System.Collections.Generic; 
+using System.IO; 
+using System.Linq; 
+
+class Day9Recursion
+{
+    public void Run(String[] args)
+    {
+        do
+        {
+            int number = Int32.Parse(Console.ReadLine()); 
+
+            var result = factorial(number); 
+            Console.WriteLine($"{result} is the factorial of {number}"); 
+        }
+        while (1 == 1); 
+    }
+
+    public int factorial(int n)
+    {
+        if (n == 1)return n; 
+        return n * factorial(n-1); 
+    }
+}
+
--- language-experiments/c#/katas/hackerrank/HackerRank/30 day challenge/Program.cs
+++ language-experiments/c#/katas/hackerrank/HackerRank/30 day challenge/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        var day = new Day8();
+        var day = new Day10BinaryConversion();
         day.Run(args);
         Console.Read();
     }
